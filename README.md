9bb45002c1de5ca41ddd2ad0bcff6130ab9e6573
sironekotoro <8675420+sironekotoro@users.noreply.github.com>
2023-02-09 01:08:30 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+
+# NAME
+
+FizzBuzz - It's new $module
+
+# SYNOPSIS
+
+    use FizzBuzz;
+
+# DESCRIPTION
+
+FizzBuzz is ...
+
+# LICENSE
+
+Copyright (C) sironekotoro.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+sironekotoro <8675420+sironekotoro@users.noreply.github.com>
---
+++ lib/FizzBuzz.pm
@@ -0,0 +1,39 @@
+package FizzBuzz;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+FizzBuzz - It's new $module
+
+=head1 SYNOPSIS
+
+    use FizzBuzz;
+
+=head1 DESCRIPTION
+
+FizzBuzz is ...
+
+=head1 LICENSE
+
+Copyright (C) sironekotoro.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+sironekotoro E<lt>8675420+sironekotoro@users.noreply.github.comE<gt>
+
+=cut
+
